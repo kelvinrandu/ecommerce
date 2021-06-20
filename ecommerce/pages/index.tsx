04e8ca10814762dcd9000a4e1494a6bd690792e5
kelvinrandu <randukelvin@gmail.com>
2021-06-20 15:52:14 +0300

    renamed static to public

--- ecommerce/pages/index.tsx
+++ ecommerce/pages/index.tsx
@@ -22,7 +22,7 @@ const Index = (props: IIndexProps) => {
       </Head>
       <Header />
       <main className="main">
-        <img src="/static/aquarium.svg" alt="a" className="background-image" />
+        <img src="/public/aquarium.svg" alt="a" className="background-image" />
         <div className="promotional-message">
           <h3>REDISCOVER</h3>
           <h2>Fishkeeping</h2>
@@ -39,10 +39,10 @@ const Index = (props: IIndexProps) => {
 Index.getInitialProps = async () => {
   return {
     products: [
-      {id: "nextjs_halfmoon", name: "Halfmoon Betta", price: 25.00, image: "../static/halfmoon.jpg", description: "The Halfmoon betta is arguably one of the prettiest betta species. It is recognized by its large tail that can flare up to 180 degrees."} as IProduct,
-      {id: "nextjs_dragonscale", name: "Dragon Scale Betta", price: 35, image: "../static/dragonscale.jpg",description: "The dragon scale betta is a rarer and higher maintenance fish. It is named by its thick white scales covering his sides that looks like dragon scale armor."} as IProduct,
-      {id: "nextjs_crowntail", name: "Crowntail Betta", price: 7.50, image: "../static/crowntail.jpg", description: "The crowntail is pretty common, but interesting none the less. It's recognized by the shape of its tail that has an appearance of a comb."} as IProduct,
-      {id: "nextjs_veiltail", name: "Veiltail Betta", price: 5.00, image: "../static/veiltail.jpg", description: "By far the most common betta fish. You can recognize it by its long tail aiming downwards."} as IProduct,
+      {id: "nextjs_halfmoon", name: "Halfmoon Betta", price: 25.00, image: "../public/halfmoon.jpg", description: "The Halfmoon betta is arguably one of the prettiest betta species. It is recognized by its large tail that can flare up to 180 degrees."} as IProduct,
+      {id: "nextjs_dragonscale", name: "Dragon Scale Betta", price: 35, image: "../public/dragonscale.jpg",description: "The dragon scale betta is a rarer and higher maintenance fish. It is named by its thick white scales covering his sides that looks like dragon scale armor."} as IProduct,
+      {id: "nextjs_crowntail", name: "Crowntail Betta", price: 7.50, image: "../public/crowntail.jpg", description: "The crowntail is pretty common, but interesting none the less. It's recognized by the shape of its tail that has an appearance of a comb."} as IProduct,
+      {id: "nextjs_veiltail", name: "Veiltail Betta", price: 5.00, image: "../public/veiltail.jpg", description: "By far the most common betta fish. You can recognize it by its long tail aiming downwards."} as IProduct,
     ]
   }
 }
